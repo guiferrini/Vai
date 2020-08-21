3ff474f168f45f17ac1753e316ff99bbcfcd0d35
Guilherme Ferrini <guilhermemferrini@gmail.com>
2020-08-21 18:51:08 -0300

    Add /blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
-import React from "react"
+// import React from "react"
 
-export default function Home() {
-  return <div>Hello world!</div>
+// export default function Home() {
+//   return <div>Hello world!</div>
+// }
+import React from 'react'
+import { Link } from 'gatsby'
+
+export default () => {
+  return (
+    <>
+      Hello world!
+      <p><Link to="/blog">View Blog</Link></p>
+    </>)
 }
