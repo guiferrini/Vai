a6de9fb261502b31d5a549efa7c6dcce05afcfde
Guilherme Ferrini <guilhermemferrini@gmail.com>
2020-08-31 16:50:35 -0300

    Add Principal: Title and body - test

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,12 +6,14 @@
 import React from 'react'
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Principal from '../../Principal/infos.json';
 
 export default () => {
   return (
     <>
       <Header />
-      
+      {Principal.title}
+      {Principal.body}
       <main>
         <section
           style={{ 
